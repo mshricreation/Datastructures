695dae6f9f289cb418dace59b4241a1670ebfe66
Manu Shrivastava <94425448+mshricreation@users.noreply.github.com>
2025-02-19 09:11:22 +0530

    lab

---
+++ linkedlist/create.c
@@ -0,0 +1,12 @@
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
